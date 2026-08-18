deafa50cffc3bc7d78ece189eb2ae0fd482352ab
Tienptv <tienptv94@gmail.com>
2026-08-18 22:55:17 +0700

    up

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteRestart from 'vite-plugin-restart'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      ViteRestart({ restart: ['.env'] })
+    ],
     server: {
       proxy: {
         '/api': {
